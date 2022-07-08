b73a9b41eae8cc28bc53805ba8930bfbb7eaf880
Ankit jangir <khati7732@gmail.com>
2022-07-09 00:55:51 +0530

    filter

---
+++ src/components/Filter.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Filter() {
+  return (
+    <div className="grid  grid-cols-5  bg-white m-4 items-center ">
+      <div className="grid border-r-2 m-4">Location</div>
+      <div className="grid border-r-2 m-4">When</div>
+      <div className="grid border-r-2 m-4">Price</div>
+      <div className="grid border-r-2 m-4">Property Type</div>
+      <div className="gird">
+        <button className="m-2 px-5  text-white border-solid bg-indigo-500 rounded-md hover:cursor-pointer ">
+          Search
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Filter;
